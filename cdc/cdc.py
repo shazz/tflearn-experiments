ffb6c32ba36e740bea8d0c9b2b5143370da9dc3b
shazz <github@metaverse.fr>
2016-10-12 21:15:13 -0400

    Fix thank to Aymeric !

--- cdc/cdc.py
+++ cdc/cdc.py
@@ -1,7 +1,8 @@
 #
 # Lesson #1 challenge from Sirajology - Introduction - Learn Python for Data Science #1
 # https://www.youtube.com/watch?v=T5pRlIbr6gg
-# Added a few classifiers
+# Ported to tf learn
+#
 # Using CDC BRFSS Annual Survey Data 2015 
 # https://www.cdc.gov/brfss/annual_data/annual_2015.html
 
@@ -11,6 +12,7 @@
 # classifier
 import tflearn
 import data_importer
+import numpy as np
 
 X, Y = data_importer.load_data(0)    
 print("X", X.shape, "Y", Y.shape)
@@ -25,7 +27,11 @@ net = tflearn.regression(net)
 # Define model
 model = tflearn.DNN(net)
 
-print("toto")
 # Start training (apply gradient descent algorithm)
-model.fit(X, Y, n_epoch=10, batch_size=1, show_metric=True)
+model.fit(X, Y, n_epoch=4, batch_size=200, show_metric=True, validation_set=0.3)
+
+pred = model.predict([[83, 183], [40, 144]])
+print("a person of 83 kg 183 cm has a probability of", pred[0][1]*100, "% to be a man")
+print("a person of 40 kg 144 cm has a probability of", pred[1][1]*100, "% to be a man")
+
 
